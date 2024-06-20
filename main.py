ceb6ac938f17b2d43316c07daf089805ad54685b
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-06-20 17:56:49 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from discord import Client, Intents
+i = Intents.default()
+i.message_content = True
+bot = Client(intents = i)
+
+@bot.event
+async def on_message(ctx):
+    if not ctx.author.bot:
+        if ctx.content == "( ・∇・)":
+            res = "( ・∇・)"
+        elif ctx.content.startswith("オウム返し"):
+            res = ctx.content.removeprefix('オウム返し')
+            if not res:
+                res = "えい！"
+        elif ctx.channel.id == channel_id:
+            match ctx.content:
+                case "すいちゃんは〜？":
+                    res = "今日も可愛い〜！"
+                case "ミオしゃ":
+                    res = "うちうち！うちだよ！大神ミオだよ〜！"
+        elif "えい！" in ctx.content:
+            res = "えい！"
+        await ctx.channel.send(res)
+bot.run("")
