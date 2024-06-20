0784ba742af2bba9c0792205baff1d4b031aa607
ss2552 <85072753+ss2552@users.noreply.github.com>
2024-06-20 18:06:08 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ async def on_message(ctx):
             res = ctx.content.removeprefix('オウム返し')
             if not res:
                 res = "えい！"
-        elif ctx.channel.id == channel_id:
+        elif ctx.channel.id == env.CHANNEL_ID:
             match ctx.content:
                 case "すいちゃんは〜？":
                     res = "今日も可愛い〜！"
@@ -21,4 +21,4 @@ async def on_message(ctx):
         elif "えい！" in ctx.content:
             res = "えい！"
         await ctx.channel.send(res)
-bot.run("")
+bot.run(env.TOKEN)
